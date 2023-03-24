d773ff2123ccc48d8244b597b184f7b6059291d6
BigBen3 <benjamin.s.sandler@gmail.com>
2023-03-23 18:01:08 -0700

    stuff

---
+++ src/components/Authenticate.svelte
@@ -0,0 +1,31 @@
+<div class="auth-container">
+    <form>
+        <h1>Login</h1>
+        <label>
+            <input type="email" placeholder="email" />
+        </label>
+        <label>
+            <input type="email" placeholder="email" />
+        </label>
+        <label>
+            <input type="email" placeholder="email" />
+        </label>
+    </form>
+</div>
+
+<style>
+    .auth-container {
+        display: flex;
+        flex-direction: column;
+        align-items:center;
+        justify-content: center;
+    } 
+    form {
+        display:flex;
+        flex-direction: column;
+        gap: 8px;
+        
+    }
+
+   
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,15 @@
-<div>
+<div class="mainContainer">
     <slot />
 </div>
+
+
+<style>
+    .mainContainer{ 
+            min-height: 100vh;
+            background: linear-gradient(to right, #000428, #000046);
+            color: white;
+            position: relative;
+            display: flex;
+            flex-direction: column;
+    }
+</style>
