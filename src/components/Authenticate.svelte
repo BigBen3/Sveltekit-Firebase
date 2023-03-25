d0456f7472ac23db3bf321e3b575098390eb2f23
BigBen3 <benjamin.s.sandler@gmail.com>
2023-03-25 15:59:25 -0700

    more changes

--- src/components/Authenticate.svelte
+++ src/components/Authenticate.svelte
@@ -1,15 +1,27 @@
+<script>
+    let email = "";
+    let password = "";
+    let  confirmPass = "";
+
+</script>
+
 <div class="auth-container">
     <form>
         <h1>Login</h1>
+        <!--if email exist then it is above else it will be center-->
+        <p>class={email ? 'above' : 'center'}</p>
         <label>
-            <input type="email" placeholder="email" />
+            <input bind:value={email} type="email" placeholder="Email" />
         </label>
         <label>
-            <input type="email" placeholder="email" />
+            <p>class={email ? 'above' : 'center'}</p>
+            <input bind:value={password} type="password" placeholder="Password" />
         </label>
         <label>
-            <input type="email" placeholder="email" />
+            <p>class={email ? 'above' : 'center'}</p>
+            <input bind:value={confirmPass} type="password" placeholder="Confirm Password" />
         </label>
+        <button type="button">Submit</button>
     </form>
 </div>
 
@@ -25,19 +37,58 @@
     form {
         display: flex;
         flex-direction: column;
-        gap: 8px;
+        gap: 14px;
         width: 400px;
         max-width: 100%;
         margin: 0 auto;
     }
 
-    form input{
-        width: 100%;
-    }
+   
 
     h1{
         text-align: center;
+        font-size: 3rem;
+    }
+    
+    form label {
+        position: relative;
+        border: 1px solid blue;
+        border-radius: 5px;
     }
 
-   
+    form input { 
+        width: 100%;
+        border: none;
+        background: transparent;
+        color: white;
+        padding: 14px;
+    
+    }
+
+    form input:foucs {
+        border: none;
+        outline: none;
+    }
+
+    form button {
+        background:navy;
+        color: white;
+        border: none;
+        padding: 14px 0;
+        border-radius: 5px;
+        cursor: pointer;
+        font-size: 1rem;
+    }
+
+    form button:hover {
+        background: blue;
+    }
+    .above, 
+    .center{
+        position: absolute;
+        transform: f translateY(-50%);
+        pointer-events: none;
+        color: white;
+        border-radius: 4px;
+    }
 </style>
