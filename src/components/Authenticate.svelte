12e32a0a5a2bce6f2315278d3a568a5ca15d06fd
BigBen3 <benjamin.s.sandler@gmail.com>
2023-03-26 15:06:16 -0700

    finished auth page now on dashboard

--- src/components/Authenticate.svelte
+++ src/components/Authenticate.svelte
@@ -4,7 +4,7 @@
     let  confirmPass = "";
     let error = false;
     let register = false;
-    
+
 
     const handleAuthenticate = () => {
         if(!email || !password || (register && !confirmPass ) ){
@@ -12,6 +12,12 @@
             return;
         }
     }
+
+    const handleRegister = () => {
+        register = !register;
+    }
+
+  
 </script>
 
 <div class="auth-container">
@@ -19,10 +25,11 @@
         <!--if reigester true make it say register else make it say login -->
         <h1>{register ? "Register" : "Login"}</h1>
         {#if error}
-        <p class="error">The information you have entered is not correct</p>
+             <p class="error">The information you have entered is not correct</p>
         {/if}
+          
         <label>
-            <!--if email exist then it is above else it will be center-->
+             <!--if email exist then it is above else it will be center-->
             <p class={email ? " above" : " center"}>Email</p>
             <input bind:value={email} type="email" placeholder="Email" />
         </label>
@@ -30,148 +37,165 @@
             <p class={password ? " above" : " center"}>Password</p>
             <input bind:value={password} type="password" placeholder="Password" />
         </label>
+        {#if register}
         <label>
             <p class={confirmPass ? " above" : " center"}>
                 Confirm Password
             </p>
             <input bind:value={confirmPass} type="password" placeholder="Confirm Password" />
         </label>
+        {/if}
         <button type="button">Submit</button>
-        <div class="options"> 
-            <p>Or</p>
-            {#if register}
-                <div>
-                    <p>Already have an account?</p>
-                    <p>Login</p>
-                </div>
-            {:else}
-            <div>
-                <p>Don't have an account?</p>
-                <p>Register</p>
-            </div>
-            {/if}
-        </div>
     </form>
+    <div class="options"> 
+        <p>Or</p>
+        {#if register}
+            <div>
+                <p>Already have an account?</p>
+                <p on:click={handleRegister} on:keydown={() => {}}>Login</p>
+            </div>
+        {:else}
+        <div>
+            <p>Don't have an account?</p>
+            <!--since it is not a button it needs an on keydown -->
+            <p on:click={handleRegister} on:keydown={() => {}}>Register</p>
+        </div>
+        {/if}
+    </div>
 </div>
 
 <style>
-    .auth-container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        flex: 1;
-        padding: 24px;
-    } 
-    form {
-        display: flex;
-        flex-direction: column;
-        gap: 14px;
-        width: 400px;
-        max-width: 100%;
-        margin: 0 auto;
-    }
+        .auth-container{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            flex: 1;
+            padding: 24px;
+        }
+        form {
+            display: flex;
+            flex-direction: column;
+            gap: 14px;
+        }
+        form,
+        .options {
+            width: 400px;
+            max-width: 100%;
+            margin: 0 auto;
+        }
+        form input {
+            width: 100%;
+        }
+        h1 {
+            text-align: center;
+            font-size: 3rem;
+        }
+        form label {
+            position: relative;
+            border: 1px solid navy;
+            border-radius: 5px;
+        }
+        form input {
+            border: none;
+            background: transparent;
+            color: white;
+            padding: 14px;
+        }
+        form input:focus {
+            border: none;
+            outline: none;
+        }
+        form label:focus-within {
+            border-color: blue;
+        }
+        form button {
+            background: navy;
+            color: white;
+            border: none;
+            padding: 14px 0;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 1rem;
+            display: grid;
+            place-items: center;
+        }
+        form button:hover {
+            background: blue;
+        }
+        .above,
+        .center {
+            position: absolute;
+            transform: translateY(-50%);
+            pointer-events: none;
+            color: white;
+            border-radius: 4px;
+            padding: 0 6px;
+            font-size: 0.8rem;
+        }
+        .above {
+            top: 0;
+            left: 24px;
+            background: navy;
+            border: 1px solid blue;
+            font-size: 0.7rem;
+        }
+        .center {
+            top: 50%;
+            left: 6px;
+            border: 1px solid transparent;
+            opacity: 0;
+        }
+        .error {
+            color: coral;
+            font-size: 0.9rem;
+            text-align: center;
+        }
+        .options {
+            padding: 14px 0;
+            overflow: hidden;
+            font-size: 0.9rem;
+            display: flex;
+            flex-direction: column;
+            gap: 4px;
+        }
+        .options > p {
+            position: relative;
+            text-align: center;
+            width: fit-content;
+            margin: 0 auto;
+            padding: 0 8px;
+        }
+        .options > p::after,
+        .options > p::before {
+            position: absolute;
+            content: "";
+            top: 50%;
+            transform: translateY(-50%);
+            width: 100vw;
+            height: 1.5px;
+            background: white;
+        }
+        .options > p::after {
+            right: 100%;
+        }
+        .options > p::before {
+            left: 100%;
+        }
 
-   
+        .options div {
+            display: flex; 
+            align-items: center;
+            gap: 8px;
+            justify-content: center;
+        }
+        
 
-    h1{
-        text-align: center;
-        font-size: 3rem;
-    }
-    
-    form label {
-        position: relative;
-        border: 1px solid blue;
-        border-radius: 5px;
-    }
+        .options div p:last-of-type {
+            color: cyan;
+            cursor: pointer;
+        }
 
-    form input { 
-        width: 100%;
-        border: none;
-        background: transparent;
-        color: white;
-        padding: 14px;
-    
-    }
-
-    form input:foucs {
-        border: none;
-        outline: none;
-    }
-
-    form label:focus-within {
-        border-color: blue;
-    }
-    form button {
-        background:navy;
-        color: white;
-        border: none;
-        padding: 14px 0;
-        border-radius: 5px;
-        cursor: pointer;
-        font-size: 1rem;
-    }
-
-    form button:hover {
-        background: blue;
-    }
-
-    .above, 
-    .center{
-        position: absolute;
-        transform: translateY(-50%);
-        pointer-events: none;
-        color: white;
-        border-radius: 4px;
-        padding: 0 6px;
-        font-size: 0.8rem;
-    }
-
-    .above {
-        top: 0;
-        left: 24px;
-        background: navy;
-        border: 1px  solid blue;
-        font-size: 0.7rem;
-    }
-
-    .center {
-        top: 50%;
-        left: 6px;
-        border: 1px solid transparent;
-        opacity:  0;
-    }
-    
-    .error {
-        color: coral;
-        font-size: 0.9rem;
-    }
-
-    .options {
-        padding: 14px 0;
-    
-    }
-        /* 
-        this makes it the first paragraph in the options div
-        */
-     .options > p {
-        position: relative;
-     }
-
-     .options > p::after,
-     .options > p::before {
-        position: absolute;
-        top: 50%;
-        left: 0;
-        transform: translateY(-50%);
-        width: 100px;
-        height: 1.5px;
-        background: white;
-     }
-
-     .options > p::after {
-        right: 100%;
-     }
 
      
 </style>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,54 @@
+<div class="mainContainer">
+    <div class="headerContainer">
+        <h1>Todo List</h1>
+        <button>Save</button>
+    </div>
+    <main>
+        <div class="enterTodo">
+            <input type="texst" placeholder="Enter todo"/>
+            <button>ADD</button>
+        </div>
+    </main>
+</div>
+
+<style>
+    .mainContainer {
+        display: flex;
+        flex-direction:column;
+        min-height: 100vh;
+        gap: 24px;
+        padding: 24px;
+    }
+
+    .headerContainer {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    
+    main {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+        flex: 1;
+    }
+
+    .enterTodo {
+        display: flex;
+        align-items:  stretch;
+    }
+
+    
+    .enterTodo input{
+        background: transparent;
+        border: none;
+        padding: 14px;
+        color: white;
+        flex: 1;
+    }
+    
+    .enterTodo input:focus{
+        outline: none;
+    }
+    
+</style>
