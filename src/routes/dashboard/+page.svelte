f0fef432f94c94c67d08b3e0d3aa94754049b5c4
BigBen3 <benjamin.s.sandler@gmail.com>
2023-03-26 20:24:52 -0700

    working on dashboard page

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,14 +1,36 @@
+<script>
+    let todoList = ['Do the groceries'];
+    let currTodo = '';
+    let error =  true;
+    
+
+    const addToto = () => {
+        error = false;
+        if(!currTodo){
+            error = true;
+        }
+        todoList.push(currTodo);
+    }
+</script>
+
+
 <div class="mainContainer">
     <div class="headerContainer">
         <h1>Todo List</h1>
-        <button>Save</button>
+        <button><i class="fa-regular fa-floppy-disk"></i><p>Save</p></button>
     </div>
     <main>
-        <div class="enterTodo">
-            <input type="texst" placeholder="Enter todo"/>
-            <button>ADD</button>
-        </div>
+       {#each todoList as todo, index }
+            <div class="todo">
+                {index+1}. {todo} 
+            </div>
+         
+       {/each}
     </main>
+    <div class="enterTodo">
+        <input bind:value={currTodo} type="text" placeholder="Enter todo"/>
+        <button>ADD</button>
+    </div>
 </div>
 
 <style>
@@ -26,29 +48,62 @@
         justify-content: space-between;
     }
     
+  
+    .headerContainer button {
+        background: #003c5b;
+        color: white;
+        padding: 10px 18px;
+        border: none;
+        border-radius: 4px;
+        font-weight: 700;
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        cursor: pointer;
+    }
+
+    .headerContainer button i {
+        font-size: 1.1rem;
+    }
+
+    .headerContainer button:hover {
+        opacity: 0.7;
+    }
+
     main {
         display: flex;
         flex-direction: column;
         gap: 8px;
         flex: 1;
     }
-
     .enterTodo {
         display: flex;
-        align-items:  stretch;
+        align-items: stretch;
+        border: 1px solid #0891b2;
+        border-radius: 5px;
+        overflow: hidden;
     }
-
     
-    .enterTodo input{
+    .enterTodo input {
         background: transparent;
         border: none;
         padding: 14px;
         color: white;
         flex: 1;
     }
-    
-    .enterTodo input:focus{
+    .enterTodo input:focus {
         outline: none;
     }
-    
+    .enterTodo button {
+        padding: 0 28px;
+        background: #003c5b;
+        border: none;
+        color: cyan;
+        font-weight: 600;
+        cursor: pointer;
+    }
+    .enterTodo button:hover {
+        background: transparent;
+    }
+   
 </style>
