7f6aefb0b353b179154bf7ee452f8553a483563c
BigBen3 <benjamin.s.sandler@gmail.com>
2023-03-28 10:25:02 -0700

    idk

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -1,7 +1,9 @@
+// @ts-nocheck
 // Import the functions you need from the SDKs you need
 import { deleteApp, getApp, getApps, initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore';
-import  {getAuth} from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+// @ts-ignore
+import { getAuth } from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
