436fd2be2cbf650e6b5691eddc64a4c8538e461b
BigBen3 <benjamin.s.sandler@gmail.com>
2023-03-28 06:50:34 -0700

    o

--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,16 @@
- 
+// @ts-ignore
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { writable } from "svelte/store";
+import { auth } from "../lib/firebase/firebase";
+
+ export const authStore = writable ({ 
+    user: null,
+    loading: true,
+    data: {}
+      
+ })
+
+ export const authHandlers = {
+    signup: async (email, pass) => {
+        await createUserWithEmailAndPassword
+   
