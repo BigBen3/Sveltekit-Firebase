bca72a99667d894514f50aed8f6ab63fb55598cf
BigBen3 <benjamin.s.sandler@gmail.com>
2023-03-28 07:50:42 -0700

    added spinning button

--- src/store/store.js
+++ src/store/store.js
@@ -15,6 +15,8 @@ import { auth } from "../lib/firebase/firebase";
 
  export const authHandlers = {
     // @ts-ignore
+    //also what is authHandler how are you able to assign it three functions? 
+    //how does this work you can use : to assing functions 
     signup: async (email, pass) => {
         await createUserWithEmailAndPassword(auth,  email, pass)
     },
